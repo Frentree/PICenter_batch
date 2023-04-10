65d062c6a9d2635f1bf0cd5220ae884c67d07b34
thdau <info@frentree.com>
2023-04-10 17:02:55 +0900

    approvalMail Log 파일 추가

--- NHTopBatch/src/com/iopts/main/BatchMain.java
+++ NHTopBatch/src/com/iopts/main/BatchMain.java
@@ -1,7 +1,19 @@
 package com.iopts.main;
 
+import java.io.File;
+
 public class BatchMain {
+	public static String currentDir = null;
+	private static String LOGJ_PATH = null;
+	
 	public static void main(String[] args) {
+		
+		currentDir = System.getProperty("user.dir");
+		File f = new File(currentDir);
+		currentDir = f.getParent().toString();
+		
+		LOGJ_PATH = currentDir + "/conf/logbackMail.xml";
+		System.setProperty("logback.configurationFile", LOGJ_PATH);
 
 		Thread th = new Thread(new DaemonThread());
 		th.start();
