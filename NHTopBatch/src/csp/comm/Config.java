35f43ce48b7a5988d87198b43b438d31a5646638
thdau <info@frentree.com>
2023-07-13 16:34:15 +0900

    SKT - CSP XML -> JSON 이관 작업 운영서버용

--- NHTopBatch/src/csp/comm/Config.java
+++ NHTopBatch/src/csp/comm/Config.java
@@ -3,19 +3,19 @@ package csp.comm;
 //properties 에서 읽어오는 방식으로 변경 해야함
 public class Config {
 	// Domain
-	public static String domain		= "https://apihs.sktelecom.com/api/is";
+	public static String domain		= "https://apih.sktelecom.com/api/is";
 
 	// BP API Key
-	public static String apiKey		= "u8yl3gmcvcowdwjt";
+	// public static String apiKey		= "u8yl3gmcvcowdwjt";
 
 	// BP 복호화 키
-	public static String decryptKey	= "7eaonchfi2v4t77g";
+	// public static String decryptKey	= "7eaonchfi2v4t77g";
 
 	// BP API Key
-	// public static String apiKey		= "gngxaf9ygutczwjn";
+	public static String apiKey		= "gngxaf9ygutczwjn";
 	
 	// BP 복호화 키
-	// public static String decryptKey	= "8pvwuz8ua8001uts";
+	public static String decryptKey	= "8pvwuz8ua8001uts";
 
 	// Encode
 	public static String encode		= "UTF-8";
